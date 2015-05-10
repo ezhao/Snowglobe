4cf262db92c24c8d6bb80b4730a8780cf003f9a4
Emily Zhao <emily@dropbox.com>
2015-05-10 14:30:52 -0700

    use a png mask instead of a programmatic mask

---
+++ app/src/main/java/com/herokuapp/ezhao/snowglobe/CutoutPngView.java
@@ -0,0 +1,43 @@
+package com.herokuapp.ezhao.snowglobe;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CutoutPngView extends View {
+    private Bitmap mask;
+    Paint maskPaint;
+
+    public CutoutPngView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        Drawable maskDrawable = getResources().getDrawable(R.drawable.mask);
+        if (maskDrawable != null) {
+            mask = ((BitmapDrawable) maskDrawable).getBitmap();
+        }
+
+        maskPaint = new Paint();
+        maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (mask != null) {
+            canvas.save();
+            canvas.drawBitmap(mask, getWidth()/2 - mask.getWidth()/2, 0, maskPaint);
+            canvas.restore();
+        }
+    }
+}
